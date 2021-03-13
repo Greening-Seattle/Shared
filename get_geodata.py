9191c70e0d2363e83bf49c2788c5a614de1f93cc
Sarah Pristash <srp137@uw.edu>
2021-03-12 19:05:52 -0800

    contains newest get_gdf function and census bounds function, can be imported

--- get_geodata.py
+++ get_geodata.py
@@ -1,3 +1,8 @@
+import pandas as pd    
+import geopandas as gpd
+import urllib.request as request
+
+
 url_list = ['https://opendata.arcgis.com/datasets/7015d5d46a284f94ac05c2ea4358bcd7_0.geojson',
             'https://opendata.arcgis.com/datasets/5fc63b2a48474100b560a7d98b5097d7_1.geojson',
             'https://opendata.arcgis.com/datasets/27af9a2485c5442bb061fa7e881d7022_2.geojson',
@@ -16,32 +21,38 @@ def get_gdf(year):
     '''Enter the desired year to download the traffic flow count
     data for that year. Example: enter '7' for the year 2007.
     '''
+
     num = year-7
     gdf_year = gpd.read_file(url_list[num])
     if year == 11:
         gdf_year = gdf_year.rename(columns={"YEAR_" : 'YEAR'})
+        gdf_year = gdf_year[gdf_year.STNAME != '16TH AVE S']
     if year == 12:
         gdf_year = gdf_year.rename(columns={'STDY_YEAR' : 'YEAR'})
     if year == 15 or year == 16:
         gdf_year = gdf_year.rename(columns={"COUNTAAWDT" : 'AAWDT', "FLOWSEGID" : "GEOBASID", 'FIRST_STNAME_ORD' : 'STNAME'})
         gdf_year = gdf_year[['AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
         if year == 15:
-            year_list = ['2015']*len(gdf_year)
+            year_list = [2015]*len(gdf_year)
             gdf_year['YEAR'] = year_list
         elif year == 16:
-            year_list = ['2016']*len(gdf_year)
+            year_list = [2016]*len(gdf_year)
             gdf_year['YEAR'] = year_list
     elif year == 17 or year == 18:
         gdf_year = gdf_year.rename(columns={"AWDT" : 'AAWDT', "FLOWSEGID" : "GEOBASID", 'STNAME_ORD' : 'STNAME'})
         gdf_year = gdf_year[['AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
         if year == 17:
-            year_list = ['2017']*len(gdf_year)
+            year_list = [2017]*len(gdf_year)
             gdf_year['YEAR'] = year_list
         elif year == 18:
-            year_list = ['2016']*len(gdf_year)
+            year_list = [2018]*len(gdf_year)
             gdf_year['YEAR'] = year_list
-    #df_year_AAWDT = df_year['AAWDT'].values
-    #df_year_geobase = df_year['GEOBASID'].values
-    #df_year_dist = df_year['SHAPE_Length'].values
     gdf_year = gdf_year[[ 'YEAR', 'AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
-    return gdf_year #, df_year_AAWDT, df_year_geobase, df_year_dist
+    gdf_year = gdf_year[gdf_year.YEAR != 0]
+    gdf_year = gdf_year[gdf_year.YEAR.notnull()]
+    return gdf_year 
+
+def get_census_bounds():
+    url = 'https://opendata.arcgis.com/datasets/de58dc3e1efc49b782ab357e044ea20c_9.geojson'
+    bounds = gpd.read_file(url)
+    return bounds
